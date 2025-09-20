9ae38df28296f78c824a3bcd63ab42e282d0539a
referr <glebsalom@gmail.com>
2025-09-21 00:32:08 +0500

    perf(label): replace MainBlit+temp texture with TextBlit to avoid allocations and improve rendering performance

--- WML.API/UI/Controls/Label.cs
+++ WML.API/UI/Controls/Label.cs
@@ -7,44 +7,21 @@ namespace WorldMachineLoader.API.UI.Controls
 {
     public class Label : Control
     {
-        public string Text
-        {
-            get => _text;
-            set
-            {
-                if (_text != value)
-                {
-                    _text = value;
-                    _dirty = true;
-                }
-            }
-        }
-        public GameColor Color { get; set; }
-        private TempTexture _texture;
-        private string _text;
-        private bool _dirty = true;
+        public string Text { get; set; }
 
         public Label(string text, Vec2 position) : base(position)
         {
             Text = text;
+            Position = new Vec2(Position.X + 2, Position.Y - 4);
         }
 
         public override void Draw(TWMTheme theme, Vec2 screenPos, byte alpha)
         {
-            Color = theme.Primary(alpha);
+            GameColor color = theme.Primary(alpha);
 
-            Game1.gMan.MainBlit(_texture, (Position + screenPos) * 2,  Color, 0, GraphicsManager.BlendMode.Normal, 1, xCentered: false);
+            Game1.gMan.TextBlit(GraphicsManager.FontType.OS, Position + screenPos, Text, color);
         }
 
-        public override void Update(Vec2 pos, bool canInteract)
-        {
-            if (_dirty || _texture == null || !_texture.isValid)
-            {
-                _texture = Game1.gMan.TempTexMan.GetSingleLineTexture(GraphicsManager.FontType.OS, _text);
-                _dirty = false;
-            }
-
-            _texture.KeepAlive();
-        }
+        public override void Update(Vec2 pos, bool canInteract) { }
     }
 }
