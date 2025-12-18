b91d507f1920f4528adde7c38afcf53b303a68a3
referr <glebsalom@gmail.com>
2025-12-18 22:48:16 +0500

    fix: Spinner UI-control now expands and shrinks based on the value's length

--- WML.API/UI/Controls/Spinner.cs
+++ WML.API/UI/Controls/Spinner.cs
@@ -1,5 +1,6 @@
 using OneShotMG;
 using OneShotMG.src.TWM;
+using OneShotMG.src.EngineSpecificCode;
 
 namespace WorldMachineLoader.API.UI.Controls
 {
@@ -16,36 +17,39 @@ namespace WorldMachineLoader.API.UI.Controls
         private ButtonIcon _decreaseButton;
         private ButtonIcon _increaseButton;
 
+        private Vec2 _valueTextSize;
+
         public Spinner(int minValue, int maxValue, int defaultValue, Vec2 position) : base(position)
         {
             MinValue = minValue;
             MaxValue = maxValue;
             Value = defaultValue;
-            _label = new Label(Value.ToString(), position + new Vec2(20, 3));
+            _label = new Label(Value.ToString(), position + new Vec2(18, 3));
             _decreaseButton = new ButtonIcon("the_world_machine/window_icons/arrow_left", new Vec2(16, 16), Position + new Vec2(0, 0), Decrease);
             _increaseButton = new ButtonIcon("the_world_machine/window_icons/arrow_right", new Vec2(16, 16), Position + new Vec2(32, 0), Increase);
         }
 
         public override void Draw(TWMTheme theme, Vec2 screenPos, byte alpha)
         {
-            _decreaseButton.Draw(theme, screenPos, alpha);
-            _increaseButton.Draw(theme, screenPos, alpha);
+            _valueTextSize = Game1.gMan.TextSize(GraphicsManager.FontType.OS, Value.ToString());
 
-            Rect rect1 = new Rect(Position.X + screenPos.X + 16, Position.Y + screenPos.Y, 16, 16);
-            Rect rect2 = new Rect(Position.X + screenPos.X + 17, Position.Y + screenPos.Y + 1, 14, 14);
+            _decreaseButton.Draw(theme, screenPos, alpha);
+            _increaseButton.Draw(theme, screenPos + new Vec2(_valueTextSize.X - 6, 0), alpha);
+
+            Rect rect1 = new Rect(Position.X + screenPos.X + 16, Position.Y + screenPos.Y, _valueTextSize.X + 10, 16);
+            Rect rect2 = new Rect(Position.X + screenPos.X + 17, Position.Y + screenPos.Y + 1, _valueTextSize.X + 8, 14);
 
             Game1.gMan.ColorBoxBlit(rect1, theme.Primary());
             Game1.gMan.ColorBoxBlit(rect2, theme.Background());
 
-            _label.Draw(theme, screenPos, alpha);
-            //Game1.gMan.TextBlit(GraphicsManager.FontType.OS, new Vec2(Position.X + screenPos.X + 22, Position.Y + screenPos.Y - 1), Value.ToString(), theme.Primary(), );
+            _label.Draw(theme, screenPos + new Vec2(1, 0), alpha);
         }
 
         public override void Update(Vec2 parentPos, bool canInteract)
         {
             _label.Update(parentPos, canInteract);
             _decreaseButton.Update(parentPos, canInteract);
-            _increaseButton.Update(parentPos, canInteract);
+            _increaseButton.Update(parentPos + new Vec2(_valueTextSize.X - 6, 0), canInteract);
         }
 
         private void Increase()
